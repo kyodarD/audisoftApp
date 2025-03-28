8fc1492666e3f0416323913aecea2fb712068629
npaezquintana@gmail.com <npaezquintana@gmail.com>
2025-03-28 16:21:18 -0500

    Actualización de rutas: moviendo rutas de productos, clientes y ventas a web.php

--- routes/api.php
+++ routes/api.php
@@ -11,10 +11,22 @@ Route::get('/usuarios/{id}', [AuthController::class, 'showUser']);
 Route::get('/usuarios', [AuthController::class, 'index']); 
 
 // Rutas de clientes
-Route::resource('clientes', ClienteController::class);
+Route::get('clientes', [ClienteController::class, 'index']);
+Route::get('clientes/{cliente}', [ClienteController::class, 'show']);
+Route::post('clientes', [ClienteController::class, 'store']);
+Route::put('clientes/{cliente}', [ClienteController::class, 'update']);
+Route::delete('clientes/{cliente}', [ClienteController::class, 'destroy']);
 
 // Rutas de productos
-Route::resource('productos', ProductoController::class);
+Route::get('productos', [ProductoController::class, 'index']);
+Route::get('productos/{producto}', [ProductoController::class, 'show']);
+Route::post('productos', [ProductoController::class, 'store']);
+Route::put('productos/{producto}', [ProductoController::class, 'update']);
+Route::delete('productos/{producto}', [ProductoController::class, 'destroy']);
 
 // Rutas de ventas
-Route::resource('ventas', VentaController::class);
+Route::get('ventas', [VentaController::class, 'index']);
+Route::get('ventas/{venta}', [VentaController::class, 'show']);
+Route::post('ventas', [VentaController::class, 'store']);
+Route::put('ventas/{venta}', [VentaController::class, 'update']);
+Route::delete('ventas/{venta}', [VentaController::class, 'destroy']);
