fd549ec53f900838c9e5970729facb6b8113214a
npaezquintana@gmail.com <npaezquintana@gmail.com>
2025-03-28 16:57:31 -0500

    Añadir la carpeta storage para persistencia de archivos

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,8 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/usuarios/{id}', [AuthController::class, 'showUser']);
 Route::get('/usuarios', [AuthController::class, 'index']); 
+Route::delete('usuarios/{id}', [AuthController::class, 'destroy']);
+
 
 // Rutas de clientes
 Route::get('clientes', [ClienteController::class, 'index']);
